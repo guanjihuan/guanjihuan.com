97d2b5c83dd87c062260b9ec22c0a76eaeaee4eb
guanjihuan <34735497+guanjihuan@users.noreply.github.com>
2021-06-24 09:35:18 +0800

    update

--- academic_codes/2021.05.21_eigenvalue_of_Kronecker_product_of_Pauli_matrices/test2_eigenvalue_of_Kronecker_product_of_Pauli_matrices.py
+++ academic_codes/2021.05.21_eigenvalue_of_Kronecker_product_of_Pauli_matrices/test2_eigenvalue_of_Kronecker_product_of_Pauli_matrices.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gjh
+import guan
 
 a_00 = np.random.uniform(-1, 1)
 a_0x = np.random.uniform(-1, 1)
@@ -22,20 +22,20 @@ a_zy = np.random.uniform(-1, 1)
 a_zz = np.random.uniform(-1, 1)
 
 hamiltonian_1 = \
-    a_00*gjh.sigma_00()+a_0x*gjh.sigma_0x()+a_0y*gjh.sigma_0y()+a_0z*gjh.sigma_0z()+ \
-    a_x0*gjh.sigma_x0()+a_xx*gjh.sigma_xx()+a_xy*gjh.sigma_xy()+a_xz*gjh.sigma_xz()+ \
-    a_y0*gjh.sigma_y0()+a_yx*gjh.sigma_yx()+a_yy*gjh.sigma_yy()+a_yz*gjh.sigma_yz()+ \
-    a_z0*gjh.sigma_z0()+a_zx*gjh.sigma_zx()+a_zy*gjh.sigma_zy()+a_zz*gjh.sigma_zz()
-eigenvalue_1 = gjh.calculate_eigenvalue(hamiltonian_1)
+    a_00*guan.sigma_00()+a_0x*guan.sigma_0x()+a_0y*guan.sigma_0y()+a_0z*guan.sigma_0z()+ \
+    a_x0*guan.sigma_x0()+a_xx*guan.sigma_xx()+a_xy*guan.sigma_xy()+a_xz*guan.sigma_xz()+ \
+    a_y0*guan.sigma_y0()+a_yx*guan.sigma_yx()+a_yy*guan.sigma_yy()+a_yz*guan.sigma_yz()+ \
+    a_z0*guan.sigma_z0()+a_zx*guan.sigma_zx()+a_zy*guan.sigma_zy()+a_zz*guan.sigma_zz()
+eigenvalue_1 = guan.calculate_eigenvalue(hamiltonian_1)
 
 
-# only gjh.sigma_0x() is changed to gjh.sigma_x0()
+# only guan.sigma_0x() is changed to guan.sigma_x0()
 hamiltonian_3 = \
-    a_00*gjh.sigma_00()+a_0x*gjh.sigma_x0()+a_0y*gjh.sigma_0y()+a_0z*gjh.sigma_0z()+ \
-    a_x0*gjh.sigma_x0()+a_xx*gjh.sigma_xx()+a_xy*gjh.sigma_xy()+a_xz*gjh.sigma_xz()+ \
-    a_y0*gjh.sigma_y0()+a_yx*gjh.sigma_yx()+a_yy*gjh.sigma_yy()+a_yz*gjh.sigma_yz()+ \
-    a_z0*gjh.sigma_z0()+a_zx*gjh.sigma_zx()+a_zy*gjh.sigma_zy()+a_zz*gjh.sigma_zz()
-eigenvalue_3 = gjh.calculate_eigenvalue(hamiltonian_3)
+    a_00*guan.sigma_00()+a_0x*guan.sigma_x0()+a_0y*guan.sigma_0y()+a_0z*guan.sigma_0z()+ \
+    a_x0*guan.sigma_x0()+a_xx*guan.sigma_xx()+a_xy*guan.sigma_xy()+a_xz*guan.sigma_xz()+ \
+    a_y0*guan.sigma_y0()+a_yx*guan.sigma_yx()+a_yy*guan.sigma_yy()+a_yz*guan.sigma_yz()+ \
+    a_z0*guan.sigma_z0()+a_zx*guan.sigma_zx()+a_zy*guan.sigma_zy()+a_zz*guan.sigma_zz()
+eigenvalue_3 = guan.calculate_eigenvalue(hamiltonian_3)
 
 print()
 print('Eigenvalue:')
