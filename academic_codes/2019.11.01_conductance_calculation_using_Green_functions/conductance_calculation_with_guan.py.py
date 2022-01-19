615f68f3d2b862ea50ed4b9d65d0e38805251654
guanjihuan <guanjihuan@163.com>
2022-01-19 21:23:22 +0800

    Update conductance_calculation_with_guan.py.py

--- academic_codes/2019.11.01_conductance_calculation_using_Green_functions/conductance_calculation_with_guan.py.py
+++ academic_codes/2019.11.01_conductance_calculation_using_Green_functions/conductance_calculation_with_guan.py.py
@@ -2,7 +2,7 @@ import guan
 import numpy as np
 
 fermi_energy_array = np.linspace(-4, 4, 400)
-h00 = guan.finite_size_along_one_direction(5)
-h01 = np.identity(5)
+h00 = guan.hamiltonian_of_finite_size_system_along_one_direction(4)
+h01 = np.identity(4)
 conductance_array = guan.calculate_conductance_with_fermi_energy_array(fermi_energy_array, h00, h01)
 guan.plot(fermi_energy_array, conductance_array, xlabel='E', ylabel='Conductance', type='-')
