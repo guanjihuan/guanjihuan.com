11d27203932891764c0ffd93218eb55a0cb875bc
guanjihuan <guanjihuan@163.com>
2022-02-18 05:46:31 +0800

    Create play_mp3_with_guan.py

---
+++ language_learning/2021.12.08_academic_words/play_mp3_with_guan.py
@@ -0,0 +1,8 @@
+# Words in webpage: https://www.guanjihuan.com/archives/4418
+# installation:
+# (1) pip install pygame
+# (2) pip install --upgrade guan
+
+import guan
+guan.play_academic_words()
+# guan.play_academic_words(bre_or_ame='ame', random_on=0, show_translation=1, show_link=1, translation_time=2, rest_time=1)
