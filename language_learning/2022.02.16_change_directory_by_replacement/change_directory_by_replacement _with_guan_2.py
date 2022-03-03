9e2ce9300044eb40e528672728c54765d45ebe39
guanjihuan <guanjihuan@163.com>
2022-03-03 16:12:47 +0800

    Create change_directory_by_replacement _with_guan_2.py

---
+++ language_learning/2022.02.16_change_directory_by_replacement/change_directory_by_replacement _with_guan_2.py
@@ -0,0 +1,4 @@
+import guan
+guan.change_directory_by_replacement(current_key_word='institution/codes', new_key_word='local/data')
+with open('data.txt', 'w') as f: # 保存数据
+    f.write('Hello world') 
