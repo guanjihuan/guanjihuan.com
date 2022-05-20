77b00904b954c49e40b9548157b6e19f0092b5af
guanjihuan <guanjihuan@163.com>
2022-05-20 14:25:42 +0800

    update

--- language_learning/2022.02.16_change_directory_by_replacement/change_directory_by_replacement.py
+++ language_learning/2022.02.16_change_directory_by_replacement/change_directory_by_replacement.py
@@ -1,7 +1,7 @@
 import os
 code_path = os.getcwd() # 当前代码文件的路径
 data_path = code_path.replace('\\', '/')  # \改为/，防止路径报错
-data_path = data_path.replace('codes', 'data') # 把路径中codes改为data
+data_path = data_path.replace('code', 'data') # 把路径中code改为data
 if os.path.exists(data_path) == False: # 如果文件夹不存在，新建文件夹
     os.makedirs(data_path)
 os.chdir(data_path) # 转到数据的存放路径
