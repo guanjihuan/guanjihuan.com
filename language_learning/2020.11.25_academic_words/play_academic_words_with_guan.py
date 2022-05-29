d8f500d9c9a84f4edb0f598be6afc7956c0da522
guanjihuan <guanjihuan@163.com>
2022-05-30 05:08:18 +0800

    Update play_academic_words_with_guan.py

--- language_learning/2020.11.25_academic_words/play_academic_words_with_guan.py
+++ language_learning/2020.11.25_academic_words/play_academic_words_with_guan.py
@@ -5,4 +5,5 @@
 
 import guan
 guan.play_academic_words()
-# guan.play_academic_words(bre_or_ame='ame', random_on=0, show_translation=1, show_link=1, translation_time=2, rest_time=1)
+# guan.play_academic_words(reverse=1)
+# guan.play_academic_words(reverse=0, random_on=0, bre_or_ame='ame', show_translation=1, show_link=1, translation_time=2, rest_time=1)
