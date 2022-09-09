5794143d620b762238eb9cd359b9ae1dea429936
guanjihuan <guanjihuan@163.com>
2022-09-09 15:59:14 +0800

    Update get_file_list_and_write_in_markdown.py

--- language_learning/2022.09.08_get_file_list_and_write_in_markdown/get_file_list_and_write_in_markdown.py
+++ language_learning/2022.09.08_get_file_list_and_write_in_markdown/get_file_list_and_write_in_markdown.py
@@ -5,7 +5,6 @@ The newest version of this code is on the web page: https://www.guanjihuan.com/a
 
 
 def main():
-    import os
     directory = 'E:/literature'
     write_file_list_in_markdown(directory)
 
@@ -20,16 +19,16 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
         if os.path.isfile(filename1_with_path):  # 文件
             if os.path.splitext(filename1)[1] not in banned_type:
                 if hide_file_type == None:
-                    f.write('+ '+str(filename1)+'\n')
+                    f.write('+ '+str(filename1)+'\n\n')
                 else:
-                    f.write('+ '+str(os.path.splitext(filename1)[0])+'\n')
+                    f.write('+ '+str(os.path.splitext(filename1)[0])+'\n\n')
         else:  # 文件夹
             u0 += 1
             if divided_line != None and u0 != 1:
                 f.write('\n--------\n\n')
             if starting_from_h1 == None:
                 f.write('#')
-            f.write('# '+str(filename1)+'\n')
+            f.write('# '+str(filename1)+'\n\n')
 
             filenames2 = os.listdir(filename1_with_path) 
             i0 = 0     
@@ -38,17 +37,17 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                 if os.path.isfile(filename2_with_path):  # 文件
                     if os.path.splitext(filename2)[1] not in banned_type:
                         if hide_file_type == None:
-                            f.write('+ '+str(filename2)+'\n')
+                            f.write('+ '+str(filename2)+'\n\n')
                         else:
-                            f.write('+ '+str(os.path.splitext(filename2)[0])+'\n')
+                            f.write('+ '+str(os.path.splitext(filename2)[0])+'\n\n')
                 else:    # 文件夹
                     i0 += 1
                     if starting_from_h1 == None:
                         f.write('#')
                     if show_second_number != None:
-                        f.write('## '+str(i0)+'. '+str(filename2)+'\n')
+                        f.write('## '+str(i0)+'. '+str(filename2)+'\n\n')
                     else:
-                        f.write('## '+str(filename2)+'\n')
+                        f.write('## '+str(filename2)+'\n\n')
                     
                     j0 = 0
                     filenames3 = os.listdir(filename2_with_path)
@@ -57,17 +56,17 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                         if os.path.isfile(filename3_with_path):    # 文件
                             if os.path.splitext(filename3)[1] not in banned_type:
                                 if hide_file_type == None:
-                                    f.write('+ '+str(filename3)+'\n')
+                                    f.write('+ '+str(filename3)+'\n\n')
                                 else:
-                                    f.write('+ '+str(os.path.splitext(filename3)[0])+'\n')
+                                    f.write('+ '+str(os.path.splitext(filename3)[0])+'\n\n')
                         else:   # 文件夹
                             j0 += 1
                             if starting_from_h1 == None:
                                 f.write('#')
                             if show_third_number != None:
-                                f.write('### ('+str(j0)+') '+str(filename3)+'\n')
+                                f.write('### ('+str(j0)+') '+str(filename3)+'\n\n')
                             else:
-                                f.write('### '+str(filename3)+'\n')
+                                f.write('### '+str(filename3)+'\n\n')
 
                             filenames4 = os.listdir(filename3_with_path)
                             for filename4 in filenames4[::reverse_positive_or_negative]:
@@ -75,13 +74,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                 if os.path.isfile(filename4_with_path):   # 文件
                                     if os.path.splitext(filename4)[1] not in banned_type:
                                         if hide_file_type == None:
-                                            f.write('+ '+str(filename4)+'\n')
+                                            f.write('+ '+str(filename4)+'\n\n')
                                         else:
-                                            f.write('+ '+str(os.path.splitext(filename4)[0])+'\n')
+                                            f.write('+ '+str(os.path.splitext(filename4)[0])+'\n\n')
                                 else:     # 文件夹
                                     if starting_from_h1 == None:
                                         f.write('#')
-                                    f.write('#### '+str(filename4)+'\n')
+                                    f.write('#### '+str(filename4)+'\n\n')
 
                                     filenames5 = os.listdir(filename4_with_path)
                                     for filename5 in filenames5[::reverse_positive_or_negative]:
@@ -89,13 +88,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                         if os.path.isfile(filename5_with_path):    # 文件
                                             if os.path.splitext(filename5)[1] not in banned_type:
                                                 if hide_file_type == None:
-                                                    f.write('+ '+str(filename5)+'\n')
+                                                    f.write('+ '+str(filename5)+'\n\n')
                                                 else:
-                                                    f.write('+ '+str(os.path.splitext(filename5)[0])+'\n')
+                                                    f.write('+ '+str(os.path.splitext(filename5)[0])+'\n\n')
                                         else:   # 文件夹
                                             if starting_from_h1 == None:
                                                 f.write('#')
-                                            f.write('##### '+str(filename5)+'\n')
+                                            f.write('##### '+str(filename5)+'\n\n')
 
                                             filenames6 = os.listdir(filename5_with_path)
                                             for filename6 in filenames6[::reverse_positive_or_negative]:
@@ -103,13 +102,13 @@ def write_file_list_in_markdown(directory, filename='a', reverse_positive_or_neg
                                                 if os.path.isfile(filename6_with_path):   # 文件
                                                     if os.path.splitext(filename6)[1] not in banned_type:
                                                         if hide_file_type == None:
-                                                            f.write('+ '+str(filename6)+'\n')
+                                                            f.write('+ '+str(filename6)+'\n\n')
                                                         else:
-                                                            f.write('+ '+str(os.path.splitext(filename6)[0])+'\n')
+                                                            f.write('+ '+str(os.path.splitext(filename6)[0])+'\n\n')
                                                 else:  # 文件夹
                                                     if starting_from_h1 == None:
                                                         f.write('#')
-                                                    f.write('###### '+str(filename6)+'\n')
+                                                    f.write('###### '+str(filename6)+'\n\n')
     f.close()
 
 
