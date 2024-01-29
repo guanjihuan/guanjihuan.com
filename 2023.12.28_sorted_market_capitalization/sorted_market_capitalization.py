342efb8562e76edfaa0d9c56e2a3e9ffdbf16519
guanjihuan <guanjihuan@163.com>
2024-01-29 21:12:33 +0800

    Update sorted_market_capitalization.py

--- 2023.12.28_sorted_market_capitalization/sorted_market_capitalization.py
+++ 2023.12.28_sorted_market_capitalization/sorted_market_capitalization.py
@@ -7,10 +7,17 @@ import akshare as ak
 import numpy as np
 stocks = ak.stock_zh_a_spot_em()
 stock_data = stocks.values
-list_index = np.argsort(stock_data[:, 17])
+new_stock_data = []
+for stock in stock_data:
+    if np.isnan(float(stock[9])):
+        continue
+    else:
+        new_stock_data.append(stock)
+new_stock_data = np.array(new_stock_data)
+list_index = np.argsort(new_stock_data[:, 17])
 list_index = list_index[::-1]
 for i0 in range(30):
-    stock_symbol = stock_data[list_index[i0], 1]
-    stock_name = stock_data[list_index[i0], 2]
-    market_capitalization = stock_data[list_index[i0], 17]/1e8
+    stock_symbol = new_stock_data[list_index[i0], 1]
+    stock_name = new_stock_data[list_index[i0], 2]
+    market_capitalization = new_stock_data[list_index[i0], 17]/1e8
     print([i0+1, stock_symbol, stock_name, market_capitalization])
