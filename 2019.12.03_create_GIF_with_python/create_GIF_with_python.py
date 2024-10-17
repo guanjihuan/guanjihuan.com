b92c89d10574b08440d4bc70084cfe816155225b
guanjihuan <guanjihuan@163.com>
2024-10-18 01:43:42 +0800

    Update create_GIF_with_python.py

--- 2019.12.03_create_GIF_with_python/create_GIF_with_python.py
+++ 2019.12.03_create_GIF_with_python/create_GIF_with_python.py
@@ -5,4 +5,8 @@ for i in range(1000):
     image = str(i)+'.jpg'
     im = imageio.imread(image)
     images.append(im)
-imageio.mimsave("a.gif", images, 'GIF', duration=0.1)  # durantion是延迟时间
+imageio.mimsave("a.gif", images, fps=5, duration=1, loop=0) 
+
+# fps: 帧率，表示每秒显示的帧数。
+# duration: 设置每一帧的持续时间（单位：秒），这里设置为 1。
+# loop: 设置动图的循环次数，0 表示无限循环。
