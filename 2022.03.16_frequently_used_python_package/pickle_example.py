5be00bb720a023bf95927b5c46807212910b5632
guanjihuan <guanjihuan@163.com>
2024-12-17 22:51:08 +0800

    Update pickle_example.py

--- 2022.03.16_frequently_used_python_package/pickle_example.py
+++ 2022.03.16_frequently_used_python_package/pickle_example.py
@@ -1,7 +1,20 @@
 import pickle
+
 data = [1, 2, 3]
+
+# 保存为文件
 with open('a.txt', 'wb') as f:
     pickle.dump(data, f)
 with open('a.txt', 'rb') as f:
-    data_load = pickle.load(f)
-print(data_load)
+    data_load_from_file = pickle.load(f)
+print(data_load_from_file)
+print()
+
+# 把对象转换成字节流
+serialized_data = pickle.dumps(data) # 转换成字节流
+print(type(serialized_data))
+print(serialized_data)
+print()
+loaded_data = pickle.loads(serialized_data) # 转换成原类型
+print(type(loaded_data))
+print(loaded_data)
