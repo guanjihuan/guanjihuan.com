0027293f2bce8e31a644a1ac81351ffeed235377
guanjihuan <guanjihuan@163.com>
2024-12-18 08:54:53 +0800

    Update ollama_with_python.py

--- 2024.12.11_ollama/ollama_with_python.py
+++ 2024.12.11_ollama/ollama_with_python.py
@@ -8,3 +8,9 @@ import ollama
 response = ollama.chat(model="llama3.2:latest", messages=[{"role": "user", "content": "你好"}], stream=True)
 for part in response:
     print(part['message']['content'], end='', flush=True)
+
+# 流式输出，且模型后台常驻（需要手动 ollama stop 关闭）
+import ollama
+response = ollama.chat(model="llama3.2:latest", messages=[{"role": "user", "content": "你好"}], stream=True, keep_alive=-1)
+for part in response:
+    print(part['message']['content'], end='', flush=True)
