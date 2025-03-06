0bcd8ce52ed8eba495f0bce2af54781a2dce35b0
guanjihuan <guanjihuan@163.com>
2025-03-07 04:15:02 +0800

    update

--- 2023.02.21_parallel_python_manually/LSF_parallel_with_guan/bsub_parallel_with_guan.py
+++ 2023.02.21_parallel_python_manually/LSF_parallel_with_guan/bsub_parallel_with_guan.py
@@ -2,6 +2,10 @@ import guan
 
 parameter_array = [1, 2, 3, 4]
 
-guan.make_sh_file_for_bsub(sh_filename='a', command_line='python a.py', cpu_num=1, task_name='task', queue_name='score', cd_dir=0)
+sh_filename = 'a'
+task_name = 'task'
+py_filename = 'a'
 
-guan.copy_py_sh_file_and_bsub_task(parameter_array, py_filename='a', old_str_in_py='parameter = 0', new_str_in_py='parameter = ', sh_filename='a', bsub_task_name='task')
+guan.make_sh_file_for_bsub(sh_filename=sh_filename, command_line=f'python {py_filename}.py', cpu_num=1, task_name=task_name, queue_name='score', cd_dir=0)
+
+guan.copy_py_sh_file_and_bsub_task(parameter_array, py_filename=py_filename, old_str_in_py='parameter = 0', new_str_in_py='parameter = ', sh_filename=sh_filename, task_name=task_name)
