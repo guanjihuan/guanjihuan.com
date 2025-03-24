1a5d874ac0005a167581d1b797ad11658927ec6c
guanjihuan <guanjihuan@163.com>
2025-03-24 18:02:45 +0800

    Update example_of_time_logging.py

--- 2024.01.16_GUAN_package_learning/common/example_of_time_logging.py
+++ 2024.01.16_GUAN_package_learning/common/example_of_time_logging.py
@@ -1,7 +1,7 @@
-# 运行统计
+# 运行时间日志
 import guan
 import time
-guan.statistics_with_day_and_time(content='start')
+guan.logging_with_day_and_time(content='start')
 for i in range(3):
     time.sleep(5)
-    guan.statistics_with_day_and_time(f'end_of_{i}')
+    guan.logging_with_day_and_time(f'end_of_{i}')
